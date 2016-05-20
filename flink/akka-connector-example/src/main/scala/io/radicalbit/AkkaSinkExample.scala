edac4a3668efa7c5066d1cb9ff66dbce3d175469
Andrea <me@alkagin.xyz>
2016-05-20 14:43:19 +0200

    [RBFDD-468] Move Cassandra and Akka examples from mvn project to isolated projects (#6)
    * [RBFDD-468] Move Cassandra and Akka examples from mvn project to isolated projects
    
    * [RBFDD-451] Document secure Flink Kafka connector (#5)
    
    * [RBFDD-451] Document secure Flink Kafka connector
    
    * Fix indentation, add Kafka dep
    
    * reformat code
    
    * [RBFDD-468] Move Cassandra and Akka examples from mvn project to isolated projects
    
    * reformat code

---
+++ flink/akka-connector-example/src/main/scala/io/radicalbit/AkkaSinkExample.scala
@@ -0,0 +1,45 @@
+package io.radicalbit
+
+import java.util.concurrent.TimeUnit
+
+import com.typesafe.config.ConfigFactory
+import org.apache.flink.connectors.akka.streaming.AkkaSink
+import org.apache.flink.streaming.api.scala._
+
+object AkkaSinkExample {
+
+  val actorReceiverPath = "akka.tcp://actor-test@127.0.0.1:4000/user/receiver"
+
+  def main(args: Array[String]): Unit = {
+
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    env.setParallelism(1)
+
+    //  AkkaSink implicit timeout
+    implicit val timeout = akka.util.Timeout(10L, TimeUnit.SECONDS)
+
+    val stream = env.generateSequence(0, 1000L).map(x => x.toString)
+    stream.addSink(new AkkaSink[String]("test", actorReceiverPath, Seq(ConfigurationUtil.conf(5000))))
+
+    env.execute("AkkaSinkExample")
+  }
+}
+
+object ConfigurationUtil {
+
+  def conf(port: Int) = ConfigFactory.parseString {
+    s"""
+        |akka {
+        |  actor {
+        |    provider = "akka.remote.RemoteActorRefProvider"
+        |  }
+        |  remote {
+        |    netty.tcp {
+        |      hostname = "127.0.0.1"
+        |      port = $port
+        |    }
+        | }
+        |}
+        |""".stripMargin
+  }
+}
